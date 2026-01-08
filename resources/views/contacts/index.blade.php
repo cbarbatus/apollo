bed65f959f834d9f3371c1699fd6d8aa4bffc999
cbarbatus <barbatus@talvola.com>
2026-01-07 22:19:33 -0800

    Removed unused style items from contacts.index

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,21 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+
     <style>
-        .table-fixed {
-            table-layout: fixed;
-            width: 100%;
-            margin: 0;
-        }
-
-        /* Fixed percentages that total exactly 100% */
-        .col-id { width: 4%; }
-        .col-name { width: 14%; }
-        .col-email { width: 18%; }
-        .col-message { width: 42%; } /* Message gets the lion's share */
-        .col-status { width: 7%; }
-        .col-action { width: 15%; }
-
         .truncate-text {
             white-space: nowrap;
             overflow: hidden;
@@ -23,14 +10,6 @@
             display: block;
             width: 100%;
         }
-
-        .break-word {
-            word-wrap: break-word;
-            white-space: normal;
-            line-height: 1.1;
-            font-size: 0.85rem; /* Slightly smaller to prevent vertical bloating */
-        }
-
     </style>
 
     <div class='container my-5'>
@@ -41,7 +20,6 @@
             <x-apollo-button color="primary" href="/contacts/spam/list">Show Spam</x-apollo-button>
             <x-apollo-button color="primary" href="/contacts/all/list">Show All</x-apollo-button>
         </div>
-
         <div>
             <x-delete-button
                 :action="url('/contacts/delete-spam')"
